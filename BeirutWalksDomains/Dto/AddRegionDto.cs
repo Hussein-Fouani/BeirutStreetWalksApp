7c685761c84b93361bd61aeb10fcb4a5bab8b40c
Hussein <41930483@students.liu.edu.lb>
2024-03-24 16:35:59 +0200

    Add AddRegionDto class

---
+++ BeirutWalksDomains/Dto/AddRegionDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BeirutWalksDomains.Dto
+{
+    public class AddRegionDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string RegionImageUrl { get; set; }
+    }
+}
