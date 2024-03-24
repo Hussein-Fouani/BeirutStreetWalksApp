7587715c406037bd5c30d7f51aebbf08776af0e5
Hussein <41930483@students.liu.edu.lb>
2024-03-24 16:35:56 +0200

    Add APIResponse class

---
+++ BeirutWalksDomains/ApiResponse/APIResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeirutWalksDomains.ApiResponse
+{
+    public class APIResponse
+    {
+        public bool IsSuccess { get; set; }
+        public object Result { get; set; }
+        public List<string> ErrorMessages{ get; set; }  = new List<string>();
+        public HttpStatusCode  StatusCode{ get; set; }
+    }
+}
